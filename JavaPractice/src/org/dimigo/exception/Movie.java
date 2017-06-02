7370cc8c4817df6cd431a410fd4a1b0de64ec35e
tjrgh <tjrgh@DESKTOP-2EQJGG0>
2017-06-02 15:25:33 +0900

    z

---
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ *   |_ Movie
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 2.
+ * </pre>
+ *
+ * @author :  tjrgh
+ * @version : 1.0
+ */
+public class Movie {
+	
+	private String title;
+	private int limitAge;
+	
+	public Movie(String title, int limitAge) {
+	
+		this.title = title;
+		this.limitAge = limitAge;
+	
+	}
+	
+	public String getTitle() {
+		
+		return title;
+	}
+	
+	public int getLimitAge() {
+	
+		return limitAge;
+	}
+	
+	public void buyTicket(int age) throws Exception {
+		
+		if (age<this.limitAge) {
+			 throw new Exception(this.getTitle()+"은/는" + this.limitAge+ "세 이상 관람가입니다.");
+		} else {
+			System.out.println(this.getTitle() + "즐감하세요.");
+		}
+	}
+}
